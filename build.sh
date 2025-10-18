7afc3641e0970dceac15225ee013f683aa0b3589
leftover <hi.leftover@qq.com>
2025-10-18 17:02:43 +0800

    修复vercel 中下载不了wget导致构建错误的问题

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
-yum install wget
+# yum install wget
+echo "start mkdir pandoc folder"
 mkdir pandoc
-wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | \
-   tar xvzf - --strip-components 1 -C ./pandoc
+echo "create pandoc folder"
+pwd
+# wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | \
+#    tar xvzf - --strip-components 1 -C ./pandoc
+tar -zxvf ./pandoc-2.11.1.1-linux-amd64.tar.gz --strip-components 1 -C ./pandoc
+ls
 export PATH="./pandoc/bin:$PATH"
+echo "Pandoc version:"
+pandoc --version
+npx --version
+npx hexo clean
+echo "complete clean"
+echo "start generate"
 npx hexo generate
+echo "complete generate"
